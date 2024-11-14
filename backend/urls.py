5eead2235c8a4b80f4970c08d2e3f9c412d20e3c
RizkyyAl-Amin <rizkyalamin7224@gmail.com>
2024-11-14 23:53:07 +0700

    second

--- backend/urls.py
+++ backend/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('dashboard',views.dashboard,name='dashboard'),
-    
+    path('login',views.login,name='login'),
+    path('art',views.art,name='art'),
+    path('kategori_artikel',views.kategori,name='kategori_artikel'),
 ]
--- backend/views.py
+++ backend/views.py
@@ -7,3 +7,21 @@ def dashboard(request):
         'title':title
     }
     return render(request, 'dashboard.html', context)
+
+def login(request):
+    context={
+        'title':title
+    }
+    return render(request, 'pages/auth/login_page.html', context)
+
+def art(request):
+    context={
+        'title':title
+    }
+    return render(request, 'pages/artikel/artikel2.html', context)
+
+def kategori(request):
+    context={
+        'title':title
+    }
+    return render(request, 'pages/artikel/kategori_artikel.html', context)
